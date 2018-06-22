d37cc55ca3b8c04f83987ff2c7238a6af595eb97
aolovely <you@example.comnguyentienthanh3010@gmail.com>
2018-06-23 00:17:05 +0700

    rocket hw3

---
+++ rocket2/src/BackGround.java
@@ -0,0 +1,65 @@
+import java.awt.*;
+
+public class BackGround {
+
+    private int positionX, positionY;
+    private int width, height;
+    private Color color;
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public BackGround(int positionX, int positionY, int width, int height, Color color) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    public BackGround() {
+
+    }
+
+    public void render(Graphics graphics){
+        graphics.setColor(this.color);
+        graphics.fillRect(this.positionX,this.positionY,width,height);
+    }
+}
